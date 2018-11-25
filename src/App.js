74486d2a53f8f78a4da8c9a1d1269dd1253b7d9c
wxmaudio <wxm224@163.com>
2018-11-25 22:23:49 +0800

    添加路由

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  {HashRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import Home from './views/Home.js';
+import Profile from './views/Profile.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Link to="/">首页</Link>
+          <Link to="/profile">个人</Link>
+         <Switch>
+           <Route path="/" exact={true} component={Home}></Route>
+           <Route path="/profile" exact={true} component={Profile}></Route>
+         </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import propTypes from 'prop-types';
+
+export default class Counter extends Component{
+  
+  render() {
+    <div>
+      Counter
+    </div>
+  }
+}
